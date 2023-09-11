895c6075f6fb42ab65118d1e63d66fa457b5db30
Platty114 <jamesplatt02@gmail.com>
2023-09-11 13:49:07 -0600

    changed some css and theming

--- src/theme.js
+++ src/theme.js
@@ -4,12 +4,35 @@ export const themeOptions = (mode) => {
     return ({
       palette: {
         mode: mode,
-        primary: {
-          main: '#7c4dff',
-        },
-        secondary: {
-          main: '#f50057',
-        },
-      },
-    })
+        ...(mode === 'dark'
+          ? {
+              primary: {
+                main: '#6544E9',
+              },
+              secondary: {
+                main: '#f50057',
+              },
+              background: {
+                default: '#192231',
+                paper: '#130C22'
+              },
+              text: {
+                primary: '#E8E4F6',
+                
+              },
+          }
+        : {
+          primary: {
+            main: '#7c4dff',
+          },
+          secondary: {
+            main: '#f50057',
+          },
+          background: {
+            default: '#192231',
+            paper: 'white'
+          }
+        }),
+    }
+  })
 };
